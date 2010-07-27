66b0f904eaeb18c2702b31bcc38b0113249d901e
Dmitry Baranovskiy <Dmitry@Baranovskiy.com>
2010-07-27 14:09:42 +1000

    v. 0.1.2 Total rewrite. :)

--- eve.js
+++ eve.js
@@ -5,108 +5,103 @@
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
  */
  
- 
-eve = (function (oldEve, wasit, objectsMap, id, events) {
-    var Eve = function (id, host) {
-            this.id = id;
-            this.host = host;
-        },
-        newEve = function (obj) {
-            var oid;
-            for (var i in objectsMap) {
-                if (objectsMap.hasOwnProperty(i) && objectsMap[i] === obj) {
-                    oid = i;
-                    break;
+eve = (function () {
+    var version = "0.1.2",
+        events = {n: {}},
+        eve = function (name, scope) {
+            var names = name.split("."),
+                e = events,
+                args = Array.prototype.slice.call(arguments, 2),
+                errors = [];
+            for (var i = 0, ii = names.length; i < ii; i++) {
+                e = e.n;
+                if (!e[names[i]]) {
+                    return [{error: "Name doesn\u2019t exist"}];
                 }
+                e = e[names[i]];
             }
-            oid = oid || id++;
-            objectsMap[oid] = obj;
-            return new Eve(oid, obj);
-        };
-
-    function binder(command) {
-        return function (name, handler) {
-            if (name && handler) {
-                var e = events[this.id] = events[this.id] || {};
-                (e[name] = e[name] || [])[command](handler);
+            while (e.n) {
+                if (e.f) {
+                    for (i = 0, ii = e.f.length; i < ii; i++) {
+                        try {
+                            e.f[i].apply(scope, args);
+                        } catch (ex) {
+                            errors.push({error: ex && ex.message || ex, func: e.f[i]});
+                        }
+                    }
+                }
+                for (var key in e.n) if (e.n.hasOwnProperty(key) && e.n[key].f) {
+                    var funcs = e.n[key].f;
+                    for (i = 0, ii = funcs.length; i < ii; i++) {
+                        try {
+                            funcs[i].apply(scope, args);
+                        } catch (e) {
+                            errors.push({error: e && e.message || e, func: funcs[i]});
+                        }
+                    }
+                }
+                e = e.n;
             }
-            return this;
+            if (errors.length) {
+                return errors;
+            }
         };
-    }
-
-    Eve.prototype.bind = binder("push");
-
-    Eve.prototype.bindOnTop = binder("unshift");
-
-    Eve.prototype.unbind = function (name, handler) {
-        if (name) {
-            var e = events[this.id],
-                handlers = e[name],
-                i = handlers && handlers.length;
-            if (handler) {
-                while (i--) {
-                    if (handlers[i] == handler) {
-                        handlers.splice(i, 1);
+    eve.on = function (name, f) {
+        var names = name.split("."),
+            e = events;
+        for (var i = 0, ii = names.length; i < ii; i++) {
+            e = e.n;
+            !e[names[i]] && (e[names[i]] = {n: {}});
+            e = e[names[i]];
+        }
+        e.f = e.f || [];
+        for (i = 0, ii = e.f.length; i < ii; i++) if (e.f[i] == f) {
+            return false;
+        }
+        e.f.push(f);
+    };
+    eve.unbind = function (name, f) {
+        var names = name.split("."),
+            e = events,
+            errors = [];
+        for (var i = 0, ii = names.length; i < ii; i++) {
+            e = e.n;
+            if (!e[names[i]]) {
+                return false;
+            }
+            e = e[names[i]];
+        }
+        while (e.n) {
+            if (f) {
+                if (e.f) {
+                    for (i = 0, ii = e.f.length; i < ii; i++) if (e.f[i] == f) {
+                        e.f.splice(i, 1);
                         break;
                     }
+                    !e.f.length && delete e.f;
                 }
-            } else {
-                handlers.length = 0;
-            }
-            if (!handlers.length) {
-                delete e[name];
-                i = 0;
-                for (var j in e) {
-                    if (e.hasOwnProperty(j)) {
-                        i++;
-                    }
-                }
-                !i && delete events[this.id] && delete objectsMap[this.id];
-            }
-        } else {
-            delete events[this.id];
-            delete objectsMap[this.id];
-        }
-        return this;
-    };
-
-    Eve.prototype.fire = function (name) {
-        if (name) {
-            var handlers = events[this.id] && events[this.id][name],
-                length = handlers && handlers.length;
-            if (length) {
-                for (var i = 0; i < length; i++) {
-                    var res = handlers[i].apply(this.host, Array.prototype.slice.call(arguments, 1)), u;
-                    if (res !== u && !res) {
+                for (var key in e.n) if (e.n.hasOwnProperty(key) && e.n[key].f) {
+                    var funcs = e.n[key].f;
+                    for (i = 0, ii = funcs.length; i < ii; i++) if (funcs[i] == f) {
+                        funcs.splice(i, 1);
                         break;
                     }
+                    !funcs.length && delete e.n[key].f;
                 }
             } else {
-                !events[this.id] && delete objectsMap[id--];
+                delete e.f;
+                for (key in e.n) if (e.n.hasOwnProperty(key) && e.n[key].f) {
+                    delete e.n[key].f;
+                }
             }
+            e = e.n;
         }
-        return this;
+        return true;
     };
-
-    newEve.version = "0.1.2";
-    newEve.toString = function () {
-        return "You are running Eve v." + this.version;
+    eve.version = version;
+    eve.toString = function () {
+        return "You are running Eve " + version;
     };
-
-    newEve.ninja = function () {
-        wasit ? (eve = oldEve) : delete eve;
-        return newEve;
-    };
-
-    function onunload() {
-        for (var i in objectsMap) {
-            delete objectsMap[i];
-            delete events[i];
-        }
-    }
-
-    window.addEventListener && window.addEventListener("unload", onunload, false);
-    window.attachEvent && window.attachEvent("onunload", onunload);
-
-    return newEve;
-})(window.eve, window.hasOwnProperty("eve"), {}, 1, {});
+    return eve;
+})();
+typeof exports != "undefined" && (exports.eve = eve);
