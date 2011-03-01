c65288a0000ea8e9037cf701018b460288e3b5e8
Dmitry Baranovskiy <Dmitry@Baranovskiy.com>
2011-03-01 22:32:36 +1100

    0.2.2 added documentation and eve.nt() method.

--- eve.js
+++ eve.js
@@ -1,21 +1,41 @@
 /*
- * Eve 0.2.1 - JavaScript Events Library
+ * Eve 0.2.2 - JavaScript Events Library
  *
  * Copyright (c) 2010 Dmitry Baranovskiy (http://dmitry.baranovskiy.com/)
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
  */
 
 var eve = (function () {
-    var version = "0.2.1",
+    var version = "0.2.2",
         has = "hasOwnProperty",
         separator = /[\.\/]/,
         wildcard = "*",
+        current_event,
         events = {n: {}},
+    /*\
+     * eve
+     ( method )
+     **
+     * Fires event with given `name`, given scope and other parameters.
+     **
+     # <strong>Arguments</strong>
+     **
+     - name (string) name of the event, dot (.) or slash (/) separated
+     - scope (object) context for the event handlers
+     - varargs (...) the rest of arguments will be sent to event handlers
+     **
+     < (array) array of errors, if any. Each element of the array is in format:
+     | {
+     |     error: // error message,
+     |     func: // handler that caused error
+     | }
+    \*/
         eve = function (name, scope) {
             var e = events,
                 args = Array.prototype.slice.call(arguments, 2),
                 listeners = eve.listeners(name),
                 errors = [];
+            current_event = name;
             for (var i = 0, ii = listeners.length; i < ii; i++) {
                 try {
                     listeners[i].apply(scope, args);
@@ -27,6 +47,18 @@ var eve = (function () {
                 return errors;
             }
         };
+    /*\
+     * eve.listeners
+     ( method )
+     **
+     * Internal method which gives you array of all event handlers that will be triggered by the given `name`.
+     **
+     # <strong>Arguments</strong>
+     **
+     - name (string) name of the event, dot (.) or slash (/) separated
+     **
+     < (array) array of event handlers
+    \*/
     eve.listeners = function (name) {
         var names = name.split(separator),
             e = events,
@@ -58,6 +90,21 @@ var eve = (function () {
         }
         return out;
     };
+    /*\
+     * eve.on
+     ( method )
+     **
+     * Binds given event handler with a given name. You can use wildcards “*” for the names:
+     | eve.on("*.under.*", f);
+     | eve("mouse.under.floor"); // triggers f
+     **
+     # <strong>Arguments</strong>
+     **
+     - name (string) name of the event, dot (.) or slash (/) separated, with optional wildcards
+     - f (function) event handler function
+     **
+     < (boolean) false if given function already assigned as event handler to given name, true otherwise
+    \*/
     eve.on = function (name, f) {
         var names = name.split(separator),
             e = events;
@@ -68,10 +115,42 @@ var eve = (function () {
         }
         e.f = e.f || [];
         for (i = 0, ii = e.f.length; i < ii; i++) if (e.f[i] == f) {
-            return false;
+            return !1;
         }
         e.f.push(f);
+        return !0;
     };
+    /*\
+     * eve.nt
+     ( method )
+     **
+     * Could be used inside event handler to figure out actual name of the event.
+     **
+     # <strong>Arguments</strong>
+     **
+     - subname (string) #optional subname of the event
+     **
+     < (string) name of the event, if `subname` is not specified
+     * or
+     < (boolean) true, if current event’s name contains `subname`
+    \*/
+    eve.nt = function (subname) {
+        if (subname) {
+            return new RegExp("(?:\\.|\\/|^)" + subname + "(?:\\.|\\/|$)").test(current_event);
+        }
+        return current_event;
+    };
+    /*\
+     * eve.unbind
+     ( method )
+     **
+     * Removes given function from the list of event listeners assigned to given name.
+     **
+     # <strong>Arguments</strong>
+     **
+     - name (string) name of the event, dot (.) or slash (/) separated, with optional wildcards
+     - f (function) event handler function
+    \*/
     eve.unbind = function (name, f) {
         var names = name.split(separator),
             e,
@@ -122,8 +201,13 @@ var eve = (function () {
                 e = e.n;
             }
         }
-        return true;
     };
+    /*\
+     * eve.version
+     ( property )
+     **
+     * Current version of the library.
+    \*/
     eve.version = version;
     eve.toString = function () {
         return "You are running Eve " + version;
