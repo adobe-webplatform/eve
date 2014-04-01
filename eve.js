2f2b0b020b0600f2c8d1d3c045f2a78c9ac4a55a
Termina1 <terminal2010@gmail.com>
2014-04-01 13:11:30 +0400

    I think there should be additional check for AMD support

--- eve.js
+++ eve.js
@@ -367,5 +367,5 @@
     eve.toString = function () {
         return "You are running Eve " + version;
     };
-    (typeof module != "undefined" && module.exports) ? (module.exports = eve) : (typeof define != "undefined" ? (define("eve", [], function() { return eve; })) : (glob.eve = eve));
+    (typeof module != "undefined" && module.exports) ? (module.exports = eve) : (typeof define != "undefined" && define.amd ? (define("eve", [], function() { return eve; })) : (glob.eve = eve));
 })(window || this);
